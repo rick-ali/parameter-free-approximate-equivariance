0211791593d7631a306a2de7590e2ad75d16acd6
rick-ali <riccardo.ali.it@gmail.com>
2025-05-22 19:41:29 +0100

    Correction

--- exploration/models/Encoders.py
+++ exploration/models/Encoders.py
@@ -42,121 +42,121 @@ class MNISTDecoder(nn.Module):
         return self.decoder(z)  # Decode to original size
     
 
-# class CIFAR10Encoder(nn.Module):
-    
-#     def __init__(self, 
-#                  num_input_channels = 3, 
-#                  base_channel_size = 32, 
-#                  latent_dim = 128, 
-#                  act_fn : object = nn.GELU):
-#         """
-#         Inputs: 
-#             - num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
-#             - base_channel_size : Number of channels we use in the first convolutional layers. Deeper layers might use a duplicate of it.
-#             - latent_dim : Dimensionality of latent representation z
-#             - act_fn : Activation function used throughout the encoder network
-#         """
-#         super().__init__()
-#         c_hid = base_channel_size
-#         self.net = nn.Sequential(
-#             nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
-#             act_fn(),
-#             nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
-#             act_fn(),
-#             nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
-#             act_fn(),
-#             nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
-#             act_fn(),
-#             nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
-#             act_fn(),
-#             nn.Flatten(), # Image grid to single feature vector
-#             nn.Linear(2*16*c_hid, latent_dim)
-#         )
-    
-#     def forward(self, x):
-#         return self.net(x)
-
-
-class BasicBlock(nn.Module):
-    def __init__(self, in_planes, out_planes, stride, dropRate=0.0):
-        super(BasicBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_planes)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
-                               padding=1, bias=False)
-        self.dropRate = dropRate
-        self.equalInOut = (in_planes == out_planes)
-        self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes,
-                                                               kernel_size=1, stride=stride,
-                                                               padding=0, bias=False) or None
-
-    def forward(self, x):
-        if not self.equalInOut:
-            x = self.relu1(self.bn1(x))
-        else:
-            out = self.relu1(self.bn1(x))
-        out = self.relu2(self.bn2(self.conv1(out if self.equalInOut else x)))
-        if self.dropRate > 0:
-            out = F.dropout(out, p=self.dropRate, training=self.training)
-        out = self.conv2(out)
-        return torch.add(x if self.equalInOut else self.convShortcut(x), out)
-
-class NetworkBlock(nn.Module):
-    def __init__(self, nb_layers, in_planes, out_planes, block, stride, dropRate=0.0):
-        super(NetworkBlock, self).__init__()
-        layers = []
-        for i in range(nb_layers):
-            layers.append(block(i == 0 and in_planes or out_planes,
-                                out_planes, i == 0 and stride or 1, dropRate))
-        self.layer = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.layer(x)
-
 class CIFAR10Encoder(nn.Module):
-    def __init__(self, latent_dim, depth=28, widen_factor=10, dropRate=0.0, num_classes=10):
-        super(CIFAR10Encoder, self).__init__()
-        assert ((depth - 4) % 6 == 0), 'Depth should be 6n+4'
-        n = (depth - 4) // 6
-        k = widen_factor
-
-        nStages = [16, 16*k, 32*k, 64*k]
-        self.conv1 = nn.Conv2d(3, nStages[0], kernel_size=3, stride=1,
-                               padding=1, bias=False)
-        self.block1 = NetworkBlock(n, nStages[0], nStages[1], BasicBlock,
-                                   stride=1, dropRate=dropRate)
-        self.block2 = NetworkBlock(n, nStages[1], nStages[2], BasicBlock,
-                                   stride=2, dropRate=dropRate)
-        self.block3 = NetworkBlock(n, nStages[2], nStages[3], BasicBlock,
-                                   stride=2, dropRate=dropRate)
-        self.bn1 = nn.BatchNorm2d(nStages[3])
-        self.relu = nn.ReLU(inplace=True)
-        self.fc = nn.Linear(nStages[3], latent_dim)
-        self.nStages = nStages
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out',
-                                        nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-
+    
+    def __init__(self, 
+                 num_input_channels = 3, 
+                 base_channel_size = 32, 
+                 latent_dim = 128, 
+                 act_fn : object = nn.GELU):
+        """
+        Inputs: 
+            - num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
+            - base_channel_size : Number of channels we use in the first convolutional layers. Deeper layers might use a duplicate of it.
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the encoder network
+        """
+        super().__init__()
+        c_hid = base_channel_size
+        self.net = nn.Sequential(
+            nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
+            act_fn(),
+            nn.Flatten(), # Image grid to single feature vector
+            nn.Linear(2*16*c_hid, latent_dim)
+        )
+    
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.block1(out)
-        out = self.block2(out)
-        out = self.block3(out)
-        out = self.relu(self.bn1(out))
-        out = F.avg_pool2d(out, 8)
-        out = out.view(-1, self.nStages[3])
-        return self.fc(out)
+        return self.net(x)
+
+
+# class BasicBlock(nn.Module):
+#     def __init__(self, in_planes, out_planes, stride, dropRate=0.0):
+#         super(BasicBlock, self).__init__()
+#         self.bn1 = nn.BatchNorm2d(in_planes)
+#         self.relu1 = nn.ReLU(inplace=True)
+#         self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+#                                padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(out_planes)
+#         self.relu2 = nn.ReLU(inplace=True)
+#         self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
+#                                padding=1, bias=False)
+#         self.dropRate = dropRate
+#         self.equalInOut = (in_planes == out_planes)
+#         self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes,
+#                                                                kernel_size=1, stride=stride,
+#                                                                padding=0, bias=False) or None
+
+#     def forward(self, x):
+#         if not self.equalInOut:
+#             x = self.relu1(self.bn1(x))
+#         else:
+#             out = self.relu1(self.bn1(x))
+#         out = self.relu2(self.bn2(self.conv1(out if self.equalInOut else x)))
+#         if self.dropRate > 0:
+#             out = F.dropout(out, p=self.dropRate, training=self.training)
+#         out = self.conv2(out)
+#         return torch.add(x if self.equalInOut else self.convShortcut(x), out)
+
+# class NetworkBlock(nn.Module):
+#     def __init__(self, nb_layers, in_planes, out_planes, block, stride, dropRate=0.0):
+#         super(NetworkBlock, self).__init__()
+#         layers = []
+#         for i in range(nb_layers):
+#             layers.append(block(i == 0 and in_planes or out_planes,
+#                                 out_planes, i == 0 and stride or 1, dropRate))
+#         self.layer = nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         return self.layer(x)
+
+# class CIFAR10Encoder(nn.Module):
+#     def __init__(self, latent_dim, depth=28, widen_factor=10, dropRate=0.0, num_classes=10):
+#         super(CIFAR10Encoder, self).__init__()
+#         assert ((depth - 4) % 6 == 0), 'Depth should be 6n+4'
+#         n = (depth - 4) // 6
+#         k = widen_factor
+
+#         nStages = [16, 16*k, 32*k, 64*k]
+#         self.conv1 = nn.Conv2d(3, nStages[0], kernel_size=3, stride=1,
+#                                padding=1, bias=False)
+#         self.block1 = NetworkBlock(n, nStages[0], nStages[1], BasicBlock,
+#                                    stride=1, dropRate=dropRate)
+#         self.block2 = NetworkBlock(n, nStages[1], nStages[2], BasicBlock,
+#                                    stride=2, dropRate=dropRate)
+#         self.block3 = NetworkBlock(n, nStages[2], nStages[3], BasicBlock,
+#                                    stride=2, dropRate=dropRate)
+#         self.bn1 = nn.BatchNorm2d(nStages[3])
+#         self.relu = nn.ReLU(inplace=True)
+#         self.fc = nn.Linear(nStages[3], latent_dim)
+#         self.nStages = nStages
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out',
+#                                         nonlinearity='relu')
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 nn.init.constant_(m.weight, 1)
+#                 nn.init.constant_(m.bias, 0)
+#             elif isinstance(m, nn.Linear):
+#                 nn.init.constant_(m.bias, 0)
+
+#     def forward(self, x):
+#         out = self.conv1(x)
+#         out = self.block1(out)
+#         out = self.block2(out)
+#         out = self.block3(out)
+#         out = self.relu(self.bn1(out))
+#         out = F.avg_pool2d(out, 8)
+#         out = out.view(-1, self.nStages[3])
+#        return self.fc(out)
 
 
 class CIFAR10Decoder(nn.Module):
